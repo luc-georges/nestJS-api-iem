de6350e5ac7570410a52c7f4054a66b938a6a650
luc georges <luc.georges.ld@gmail.com>
2022-06-16 15:09:54 +0200

    setup prisma module and services

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,8 +3,9 @@ import { AuthModule } from './auth/auth.module';
 import { UserModule } from './user/user.module';
 import { ParkingModule } from './parking/parking.module';
 import { TicketModule } from './ticket/ticket.module';
+import { PrismaModule } from './prisma/prisma.module';
 
 @Module({
-  imports: [AuthModule, UserModule, ParkingModule, TicketModule],
+  imports: [AuthModule, UserModule, ParkingModule, TicketModule, PrismaModule],
 })
 export class AppModule {}
