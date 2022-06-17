bcd7bd2fde7503b102379c24230c3c87014f6cfb
luc georges <luc.georges.ld@gmail.com>
2022-06-17 14:05:23 +0200

    setup access_token on signin

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,13 +1,19 @@
 import { ForbiddenException, Injectable } from "@nestjs/common";
+import { JwtService } from "@nestjs/jwt";
 import { PrismaService } from "src/prisma/prisma.service";
 import { AuthDto } from "./dto";
 import * as argon from "argon2"
 import { PrismaClientKnownRequestError } from "@prisma/client/runtime";
+import { ConfigService } from "@nestjs/config";
 
 
 @Injectable()
 export class AuthService {
-    constructor(private prisma: PrismaService){}
+    constructor(
+        private prisma: PrismaService, 
+        private jwt: JwtService,
+        private config: ConfigService,
+        ){}
 
     async signup(dto: AuthDto) {
         //generate hashed password
@@ -55,7 +61,28 @@ export class AuthService {
             throw new ForbiddenException(
                 'credentials incorrect',
         );
-        delete user.hash
-        return user
+
+        return this.signToken(user.id, user.email)
     }
+
+    async signToken(
+        userID:number, 
+        email: string
+        ): Promise<{ access_token : string }> {
+
+        const payload = {
+            sub: userID,
+            email,
+        }
+
+        const secret = this.config.get('JWT_SECRET')
+        const token = await this.jwt.signAsync(payload,{
+            expiresIn:'30m',
+            secret: secret
+
+        })
+        return {
+            access_token: token
+        }
+   }
 }
