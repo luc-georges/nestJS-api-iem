de3a8b947b563ced22fbd8cd5d9173b8da56ba47
luc georges <luc.georges.ld@gmail.com>
2022-06-17 13:20:22 +0200

    add dotenv

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,4 +1,6 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+
 import { AuthModule } from './auth/auth.module';
 import { UserModule } from './user/user.module';
 import { ParkingModule } from './parking/parking.module';
@@ -6,6 +8,12 @@ import { TicketModule } from './ticket/ticket.module';
 import { PrismaModule } from './prisma/prisma.module';
 
 @Module({
-  imports: [AuthModule, UserModule, ParkingModule, TicketModule, PrismaModule],
+  imports: [
+    ConfigModule.forRoot({isGlobal:true}), 
+    AuthModule, 
+    UserModule, 
+    ParkingModule, 
+    TicketModule, 
+    PrismaModule],
 })
 export class AppModule {}
